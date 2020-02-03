a4f13cd340eb9afba3fc471fd194d0b12575bbce
heartcloud22 <december22heart@gmail.com>
2020-02-03 07:18:38 +0700

    second

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", require("./routes/api/index"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
   app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
 
